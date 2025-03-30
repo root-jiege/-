709b02b28b8a1c36194dbb42560dafdf0408dc2f
root-jiege <3690713778@QQ.com>
2025-03-30 09:42:57 +0800

    Create key.lua

---
+++ key.lua
@@ -0,0 +1,27 @@
+    if game.Players.LocalPlayer.Name == "fzdfxn" then
+        mykey = "1"
+    elseif game.Players.LocalPlayer.Name == "xdfbyjcx" then
+        mykey = "user2"
+    elseif game.Players.LocalPlayer.Name == "TTTE_LBSCE2103" then
+        mykey = "user3"
+    elseif game.Players.LocalPlayer.Name == "liu6230016" then
+        mykey = "user4"
+    elseif game.Players.LocalPlayer.Name == "kan54188" then
+        mykey = "user5"
+    elseif game.Players.LocalPlayer.Name == "kkjgfthggg" then
+        mykey = "user6"
+    elseif game.Players.LocalPlayer.Name == "SSdd8567" then
+        mykey = "SBD"
+    elseif game.Players.LocalPlayer.Name == "GBDW666" then
+        mykey = "user7"
+    elseif game.Players.LocalPlayer.Name == "tttyyy369258" then
+        mykey = "user8"
+    elseif game.Players.LocalPlayer.Name == "Anunknow_fish" then
+        mykey = "user9"
+    elseif game.Players.LocalPlayer.Name == "samcZSZ" then
+        mykey = "user10"
+    --[[
+    elseif game.Players.LocalPlayer.Name == "" then
+        mykey = ""
+    ]]
+    end
