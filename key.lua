f6005fb1652e15fdccf64276651683e2b41c8eae
root-jiege <3690713778@QQ.com>
2025-04-03 21:35:59 +0800

    Update key.lua

--- key.lua
+++ key.lua
@@ -6,7 +6,7 @@ elseif game.Players.LocalPlayer.Name == "xiaoyouks2032" then
     mykey = "RS_Vip_2"
 elseif game.Players.LocalPlayer.Name == "FDHGJKFDJ" then
     mykey = "RS_Vip_3"
-elseif game.Players.LocalPlayer.Name == "fzdfxn" then
-    mykey = "1"
+elseif game.Players.LocalPlayer.Name == "MameAAAW" then
+    mykey = "RS_Vip_4"
 
 end
