51d847567a7853dcd65797ab216cc1681eaed725
root-jiege <3690713778@QQ.com>
2025-04-03 20:47:01 +0800

    Update DeadRailFunctions.lua

--- DeadRailFunctions.lua
+++ DeadRailFunctions.lua
@@ -1,6 +1,6 @@
 Name = game.Players.LocalPlayer.Name
-if Name == "" then
-  
-elseif Name == "" then
-  
+if Name == "qazjjj4" then
+  whitelisttrue = true
+elseif Name == "kukuhuitu" then
+  whitelisttrue = true
 end
